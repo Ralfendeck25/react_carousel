381ab5dedaf91a6a2125ff48da7fc4d7cda8deb0
Peterson <poseidoncenghizerzhemetmhossad@outlook.com>
2025-05-07 02:40:48 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,16 @@ import './App.scss';
 import Carousel from './components/Carousel';
 
 const images = [
-  './img/1.png',
-  './img/2.png',
-  './img/3.png',
-  './img/4.png',
-  './img/5.png',
-  './img/6.png',
-  './img/7.png',
-  './img/8.png',
-  './img/9.png',
-  './img/10.png',
+  '/img/1.png',
+  '/img/2.png',
+  '/img/3.png',
+  '/img/4.png',
+  '/img/5.png',
+  '/img/6.png',
+  '/img/7.png',
+  '/img/8.png',
+  '/img/9.png',
+  '/img/10.png',
 ];
 
 type Settings = {
